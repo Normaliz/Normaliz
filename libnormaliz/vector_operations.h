1bd73ab5d92f979850c03094d9633d682d48fed5
Christof Soeger <csoeger@uos.de>
2012-02-20 15:45:35 +0100

    compare_last comares ONLY for last element
    after the last fix the better solution

--- libnormaliz/vector_operations.h
+++ libnormaliz/vector_operations.h
@@ -129,11 +129,7 @@ int v_difference_ordered_fast(const vector<size_t>& u,const vector<size_t>& v);
 template<typename Integer>
 bool compare_last (const vector<Integer>& a, const vector<Integer>& b)
 {
-    if(a.back() < b.back())
-        return true;
-    if(a.back() > b.back())
-        return false;
-    return a<b;
+    return a.back() < b.back();
 }
 
 //returns a key vector containing the positions of non-zero entrys of v
