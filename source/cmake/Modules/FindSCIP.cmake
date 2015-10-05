7622d4fc1a67c94df93b63cc74dcc67402332f84
Christof Soeger <csoeger@uos.de>
2015-10-06 00:54:42 +0200

    FindSCIP also finds mac versions now

--- source/cmake/Modules/FindSCIP.cmake
+++ source/cmake/Modules/FindSCIP.cmake
@@ -36,7 +36,8 @@ ELSE()
   	        DOC "The directory containing the SCIP header files"
   )
 
-  FIND_LIBRARY(SCIP_LIBRARY NAMES libscipopt-3.1.1.linux.x86_64.gnu.opt.a libscipopt-3.2.0.linux.x86_64.gnu.opt.a
+  FIND_LIBRARY(SCIP_LIBRARY 
+    NAMES libscipopt-3.2.0.linux.x86_64.gnu.opt.a libscipopt-3.1.1.linux.x86_64.gnu.opt.a libscipopt-3.2.0.darwin.x86_64.gnu.opt.a  libscipopt-3.1.1.darwin.x86_64.gnu.opt.a
     HINTS ENV SCIP_LIB_DIR
           ENV SCIP_DIR
     PATH_SUFFIXES lib
