cec43c497b4128dbefdf42eb08ac1f485aa75166
Christof Soeger <csoeger@uos.de>
2016-02-03 15:13:37 +0100

    Normaliz version 3.1.0

--- source/Normaliz.cpp
+++ source/Normaliz.cpp
@@ -50,7 +50,7 @@ void printHeader() {
                                                  << "             \\....|"<<endl;
     cout << "                                                      \\...|"<<endl;
     cout << "     (C) The Normaliz Team, University of Osnabrueck   \\..|"<<endl;
-    cout << "                    September 2015                      \\.|"<<endl;
+    cout << "                     February 2016                      \\.|"<<endl;
     cout << "                                                         \\|"<<endl;
 }
 void printHelp(char* command) {
