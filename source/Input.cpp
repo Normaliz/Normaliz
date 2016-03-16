181c0ac88c97c6522552818fdae36d2878270159
Winfried Bruns <wbruns@uos.de>
2016-03-16 14:58:53 +0100

    Formatted matrices and vectors, amb_space auto

--- source/Input.cpp
+++ source/Input.cpp
@@ -127,20 +127,18 @@ void process_constraint(const string& rel, const vector<Integer>& left, Integer 
 template <typename Integer>
 bool read_modulus(istream& in, Integer& modulus) {
 
-    in >> std::ws;  // eat up any leading white space
-    if(in.peek() != '(')
-      return false;
+    in >> std::ws;  // gobble any leading white space
     char dummy;
     in >> dummy;
-    in >> modulus;
-    if(modulus==0)
+    if(dummy != '(')
       return false;
-    if(in.fail())
-      return false;;
-    in >> std::ws;  // eat up any white space before closing
-    if(in.peek() != ')')
-      return false;;
-    in >> dummy; 
+    in >> modulus;
+    if(in.fail() || modulus==0)
+        return false;
+    in >> std::ws;  // gobble any white space before closing
+    in >> dummy;
+    if(dummy != ')')
+        return false;
     return true;
 }
 
@@ -175,6 +173,69 @@ void read_constraints(istream& in, long dim, map <Type::InputType, vector< vecto
 }
 
 template <typename Integer>
+bool read_formatted_vector(istream& in, vector<Integer>& input_vec) {
+
+    input_vec.clear();
+    in >> std::ws;
+    char dummy;
+    in >> dummy; // read first proper character
+    if(dummy!='[')
+        return false;
+    bool one_more_entry_required=false;
+    while(true){
+        in >> std::ws;
+        if(!one_more_entry_required && in.peek()==']'){
+            in >> dummy;
+            return true;
+        }
+        Integer number;
+        in >> number;
+        if(in.fail())
+            return false;
+        input_vec.push_back(number);
+        in >> std::ws;
+        one_more_entry_required=false;
+        if(in.peek()==',' || in.peek()==';'){  // skip potential separator
+            in >> dummy;
+            one_more_entry_required=true;
+        }
+    }
+}
+
+template <typename Integer>
+bool read_formatted_matrix(istream& in, vector<vector<Integer> >& input_mat) {
+    input_mat.clear();
+    in >> std::ws;
+    char dummy;
+    in >> dummy; // read first proper character
+    if(dummy!='[')
+        return false;
+    bool one_more_entry_required=false;
+    while(true){
+        in >> std::ws;
+        if(!one_more_entry_required && in.peek()==']'){ // closing ] found
+            in >> dummy;
+            return true;
+        }
+        vector<Integer> input_vec;
+        if(!read_formatted_vector(in,input_vec)){
+            throw BadInputException("Error in reading input vector!");
+        }
+        if(input_mat.size()>0 && input_vec.size()!=input_mat[0].size()){
+            throw BadInputException("Rows of input matrix have unequal lengths!");
+        }            
+        input_mat.push_back(input_vec);
+        in >> std::ws;
+        one_more_entry_required=false;
+        if(in.peek()==',' || in.peek()==';'){ // skip potential separator
+            in >> dummy;
+            one_more_entry_required=true;
+        }
+    }
+}
+    
+
+template <typename Integer>
 map <Type::InputType, vector< vector<Integer> > > readNormalizInput (istream& in, OptionsHandler& options) {
 
     string type_string;
@@ -204,9 +265,22 @@ map <Type::InputType, vector< vector<Integer> > > readNormalizInput (istream& in
         if (!in.good() || type_string != "amb_space") {
             throw BadInputException("First entry must be \"amb_space\"!");
         }
-        in >> dim;
-        if (!in.good() || dim <= 0) {
-            throw BadInputException("Bad amb_space value!");
+        bool dim_known=false;
+        in >> std::ws;
+        c=in.peek();
+        if(c=='a'){
+            string dummy;
+            in >> dummy;
+            if(dummy!="auto"){
+                throw BadInputException("Bad amb_space value!");
+            }
+        }
+        else{            
+            in >> dim;
+            if (!in.good() || dim <= 0) {
+                throw BadInputException("Bad amb_space value!");
+            }
+            dim_known=true;
         }
         while (in.good()) {
             in >> std::ws;  // eat up any leading white spaces
@@ -236,60 +310,126 @@ map <Type::InputType, vector< vector<Integer> > > readNormalizInput (istream& in
                     continue;
                 }
                 if (type_string == "total_degree") {
+                    if(!dim_known){
+                        throw BadInputException("Ambient space must be known for "+type_string+"!");
+                    }
                     input_type = Type::grading;
                     save_matrix(input_map, input_type, vector< vector<Integer> >(1,vector<Integer>(dim+type_nr_columns_correction(input_type),1)));
                     continue;
                 }
                 if (type_string == "nonnegative") {
+                    if(!dim_known){
+                        throw BadInputException("Ambient space must be known for "+type_string+"!");
+                    }
                     input_type = Type::signs;
                     save_matrix(input_map, input_type, vector< vector<Integer> >(1,vector<Integer>(dim+type_nr_columns_correction(input_type),1)));
                     continue;
                 }
                 if(type_string == "constraints") {
+                    if(!dim_known){
+                        throw BadInputException("Ambient space must be known for "+type_string+"!");
+                    }
                     read_constraints(in,dim,input_map);
                     continue;
                 }
 
 
                 input_type = to_type(type_string);
+                if(dim_known)
+                    nr_columns = dim + type_nr_columns_correction(input_type);
 
                 if (type_is_vector(input_type)) {
                     nr_rows = 1;
                     in >> std::ws;  // eat up any leading white spaces
                     c = in.peek();
-                    if (!std::isdigit(c) && c != '-') {
+                    if (c=='u') { // must be unit vector
                         string vec_kind;
                         in >> vec_kind;
-                        if (vec_kind == "unit_vector") {
-                            long pos = 0;
-                            in >> pos;
-                            if (in.fail()) {
-                                throw BadInputException("Error while reading "
-                                        + type_string 
-                                        + " as a unit_vector from the input!");
-                            }
-
-                            vector< vector<Integer> > e_i = vector< vector<Integer> >(1,vector<Integer>(dim+type_nr_columns_correction(input_type),0));
-                            if (pos < 1 || pos > static_cast<long>(e_i[0].size())) {
-                                throw BadInputException("Error while reading "
-                                        + type_string + " as a unit_vector "
-                                        + toString(pos) + " from the input!");
-                            }
-                            pos--; // in input file counting starts from 1
-                            e_i[0].at(pos) = 1;
-                            save_matrix(input_map, input_type, e_i);
-                            continue;
+                        if (vec_kind != "unit_vector") {
+                            throw BadInputException("Error while reading "
+                            + type_string 
+                            + ": unit_vector expected!");                            
                         }
-                    }
-                } else {
+
+                        long pos = 0;
+                        in >> pos;
+                        if (in.fail()) {
+                            throw BadInputException("Error while reading "
+                                    + type_string 
+                                    + " as a unit_vector!");
+                        }
+                        
+                        if(!dim_known){
+                            throw BadInputException("Ambient space must be known for "+type_string+"!");
+                        }
+
+                        vector< vector<Integer> > e_i = vector< vector<Integer> >(1,vector<Integer>(nr_columns,0));
+                        if (pos < 1 || pos > static_cast<long>(e_i[0].size())) {
+                            throw BadInputException("Error while reading "
+                                    + type_string + " as a unit_vector "
+                                    + toString(pos) + "!");
+                        }
+                        pos--; // in input file counting starts from 1
+                        e_i[0].at(pos) = 1;
+                        save_matrix(input_map, input_type, e_i);
+                        continue;
+                    } // end unit vector
+                    if (c == '[') { // must be formatted vector
+                        vector<Integer> formatted_vec;
+                        bool success = read_formatted_vector(in,formatted_vec);
+                        if(!dim_known){
+                            dim=formatted_vec.size()- type_nr_columns_correction(input_type);
+                            dim_known=true;
+                            nr_columns = dim + type_nr_columns_correction(input_type);
+                        }
+                        if(!success || formatted_vec.size()!=nr_columns){
+                            throw BadInputException("Error while reading "
+                            + type_string 
+                            + " as a formatted vector!");
+                        }
+                        save_matrix(input_map, input_type, vector<vector<Integer> > (1,formatted_vec)); 
+                        continue;
+                    } // end formatted vector
+
+                } else {  // end vector, it is a matrix. Plain vector read as a one row matrix
+                    in >> std::ws;
+                    c = in.peek();
+                    if(c=='['){ // it is a formatted matrix
+                        vector<vector<Integer> > formatted_mat;
+                        bool success=read_formatted_matrix(in,formatted_mat);
+                        if(!success){
+                            throw BadInputException("Error while reading formatted matrix "
+                            + type_string + "!");    
+                        }
+                        if(formatted_mat.size() ==0) // empty matrix
+                            continue;
+                        if(!dim_known){
+                            dim=formatted_mat[0].size()- type_nr_columns_correction(input_type);
+                            dim_known=true;
+                            nr_columns = dim + type_nr_columns_correction(input_type);
+                        }
+                        
+                        if(formatted_mat[0].size()!=nr_columns){
+                            throw BadInputException("Error while reading formatted matrix "
+                                + type_string + "!");    
+                        }
+                        
+                        save_matrix(input_map, input_type, formatted_mat);
+                        continue;
+                    }  // only plain matrix left
+                    
                     in >> nr_rows;
                 }
-                nr_columns = dim + type_nr_columns_correction(input_type);
+                
+                if(!dim_known){
+                    throw BadInputException("Ambient space must be known for plain matrix or vector "+type_string+"!");
+                }
+
                 if(in.fail() || nr_rows < 0) {
                     throw BadInputException("Error while reading "
                             + type_string + " (a " + toString(nr_rows)
                             + "x" + toString(nr_columns)
-                            + " matrix) from the input!");
+                            + " matrix) !");
                 }
                 vector< vector<Integer> > M(nr_rows,vector<Integer>(nr_columns));
                 for(i=0; i<nr_rows; i++){
@@ -302,7 +442,7 @@ map <Type::InputType, vector< vector<Integer> > > readNormalizInput (istream& in
             if (in.fail()) {
                 throw BadInputException("Error while reading " + type_string
                         + " (a " + toString(nr_rows) + "x"
-                        + toString(nr_columns) + " matrix) from the input!");
+                        + toString(nr_columns) + " matrix) !");
             }
         }
     } else {
@@ -315,7 +455,7 @@ map <Type::InputType, vector< vector<Integer> > > readNormalizInput (istream& in
             if((nr_rows <0) || (nr_columns < 0)){
                 throw BadInputException("Error while reading a "
                         + toString(nr_rows) + "x" + toString(nr_columns)
-                        + " matrix from the input!");
+                        + " matrix !");
             }
             vector< vector<Integer> > M(nr_rows,vector<Integer>(nr_columns));
             for(i=0; i<nr_rows; i++){
@@ -330,7 +470,7 @@ map <Type::InputType, vector< vector<Integer> > > readNormalizInput (istream& in
             if ( in.fail() ) {
                 throw BadInputException("Error while reading a " 
                         + toString(nr_rows) + "x" + toString(nr_columns)
-                        + " matrix from the input!");
+                        + " matrix!");
             }
 
             input_type = to_type(type_string);
