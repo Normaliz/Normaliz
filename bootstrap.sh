7c9ba373ccd1b7b9edc8c1867bbb285c40e0b506
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-07-21 11:36:47 +0200

    Add --force option to autoreconf

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 ## This script is only necessary when Normaliz is checked out from git.
 ## A distribution made using "make distcheck" will contain the complete
 ## build system already, so it is ready for "./configure && make && make install".
-autoreconf -i
+autoreconf -i --force
 echo "Now you can do:"
 echo "  ./configure && make && make install"
