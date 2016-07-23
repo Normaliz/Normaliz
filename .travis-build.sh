1ffd4027dba1e003703ea641c35cebe0d6c5b365
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-07-23 04:35:40 -0700

    Test both autotools and cmake build system with travis

---
+++ .travis-build.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+case $BUILDSYSTEM in
+    cmake)
+	mkdir -p BUILD || exit 1
+	cd BUILD || exit 1
+	pwd
+	cmake ../source || exit 1
+	make -j2 || exit 1
+	OMP_NUM_THREADS=4 make check || exit 1
+	;;
+    *)
+	# autotools
+	./bootstrap.sh || exit 1
+	./configure || exit 1
+	make -j2 || exit 1
+	OMP_NUM_THREADS=4 make -j2 check || exit 1
+	make distcheck || exit 1
+	;;
+esac
