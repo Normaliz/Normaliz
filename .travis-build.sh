394fdc9c0c9ffb5019490c3c2c0bd7c17350b243
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-07-23 12:33:24 -0700

    New travis build: autotools-makedistcheck-nmzintegrate

--- .travis-build.sh
+++ .travis-build.sh
@@ -13,6 +13,28 @@ case $BUILDSYSTEM in
 	./configure || exit 1
 	make -j2 distcheck || exit 1
 	;;
+    autotools-makedistcheck-nmzintegrate)
+	# This makes sure that the distribution contains the nmzIntegrate sources
+	# and that the distribution correctly builds it when CoCoALib is installed.
+	COCOALIB_VERSION=0.99543
+	NMZDIR=`pwd`
+	rm -Rf CoCoA
+	mkdir CoCoA || exit 1
+	cd CoCoA || exit 1
+	wget http://cocoa.dima.unige.it/cocoalib/tgz/CoCoALib-$COCOALIB_VERSION.tgz || exit 1
+	tar xf CoCoALib-$COCOALIB_VERSION.tgz || exit 1
+	cd CoCoALib-$COCOALIB_VERSION || exit 1
+	COCOALIB_DIR=`pwd` || exit 1
+	./configure --threadsafe-hack || exit 1
+	make -j2 library || exit 1
+	cd $NMZDIR || exit 1
+	./bootstrap.sh || exit 1
+	# Don't pass CoCoA flags here. We want to make sure that the distribution
+	# is complete even when this source tree is not configured with nmzintegrate.
+	./configure || exit 1
+	# Rather, build the unpacked distribution with CoCoA.
+	make -j2 DISTCHECK_CONFIGURE_FLAGS="--with-cocoalib=$COCOALIB_DIR --enable-nmzintegrate" distcheck || exit 1
+	;;
     *)
 	# autotools
 	./bootstrap.sh || exit 1
