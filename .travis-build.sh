ea8722b28ebc9edb0114c274ebb3433a32a74678
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-08-11 15:32:27 -0700

    New Travis CI build system: autotools-scipshared

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,7 +1,41 @@
 #! /bin/sh
 set -e # exit on errors
+NMZDIR=`pwd`
+# Set up SCIP if necessary for this build
+SCIPOPTSUITE_VERSION=3.2.1
 case $BUILDSYSTEM in
-    cmake)
+    *-scipshared*)
+	SCIP_FLAVOR=shared
+	SCIP_SHARED=true
+	;;
+    *-scip*)
+	SCIP_FLAVOR=static
+	SCIP_SHARED=false
+	;;
+esac
+case $BUILDSYSTEM in
+    *-scip*)
+	SCIP_BUILD_DIR=build_scip_$SCIP_FLAVOR
+	if test ! -f $SCIP_BUILD_DIR/scipoptsuite-$SCIPOPTSUITE_VERSION/.completed_build ; then
+	    if test ! -f scipoptsuite-$SCIPOPTSUITE_VERSION.tgz ; then
+		wget --post-data 'tname=normaliz-travis-ci-build&license=academic' "http://scip.zib.de/download.php?fname=scipoptsuite-$SCIPOPTSUITE_VERSION.tgz" --output-document=scipoptsuite-$SCIPOPTSUITE_VERSION.tgz
+	    fi
+	    mkdir -p $SCIP_BUILD_DIR
+	    cd $SCIP_BUILD_DIR
+	    rm -Rf scipoptsuite-$SCIPOPTSUITE_VERSION
+	    tar xf $NMZDIR/scipoptsuite-$SCIPOPTSUITE_VERSION.tgz
+	    cd scipoptsuite-$SCIPOPTSUITE_VERSION
+	    # Pass our own CXXFLAGS because otherwise SoPlex 2.2.1 will use "-std=c++11",
+	    # which the old compilers on Travis CI do not support.
+	    make -j2 CXXFLAGS="-std=c++0x -fPIC" VERBOSE=true ZLIB=false GMP=false READLINE=false SHARED=$SCIP_SHARED scipoptlib
+	    touch .completed_build
+	fi
+	;;
+esac
+# Build Normaliz.
+cd $NMZDIR
+case $BUILDSYSTEM in
+    cmake*)
 	mkdir -p BUILD || exit 1
 	cd BUILD || exit 1
 	pwd
@@ -18,7 +52,6 @@ case $BUILDSYSTEM in
 	# This makes sure that the distribution contains the nmzIntegrate sources
 	# and that the distribution correctly builds it when CoCoALib is installed.
 	COCOALIB_VERSION=0.99543
-	NMZDIR=`pwd`
 	#rm -Rf CoCoA
 	COCOADIR=CoCoA
 	COCOALIB_DIR=`pwd`/$COCOADIR/CoCoALib-$COCOALIB_VERSION
@@ -40,31 +73,16 @@ case $BUILDSYSTEM in
 	# Rather, build the unpacked distribution with CoCoA.
 	make -j2 DISTCHECK_CONFIGURE_FLAGS="--with-cocoalib=$COCOALIB_DIR --enable-nmzintegrate --disable-scip --disable-shared" distcheck || ( echo '#### Contents of config.log: ####'; cat normaliz-*/_build/config.log; exit 1)
 	;;
-    autotools-scip)
-	SCIPOPTSUITE_VERSION=3.2.1
-	NMZDIR=`pwd`
-	if test ! -f scipoptsuite-$SCIPOPTSUITE_VERSION/.completed_build ; then
-	    if test ! -f scipoptsuite-$SCIPOPTSUITE_VERSION.tgz ; then
-		wget --post-data 'tname=normaliz-travis-ci-build&license=academic' "http://scip.zib.de/download.php?fname=scipoptsuite-$SCIPOPTSUITE_VERSION.tgz" --output-document=scipoptsuite-$SCIPOPTSUITE_VERSION.tgz
-	    fi
-	    rm -Rf scipoptsuite-$SCIPOPTSUITE_VERSION
-	    tar xf scipoptsuite-$SCIPOPTSUITE_VERSION.tgz
-	    cd scipoptsuite-$SCIPOPTSUITE_VERSION
-	    # Pass our own CXXFLAGS because otherwise SoPlex 2.2.1 will use "-std=c++11",
-	    # which the old compilers on Travis CI do not support.
-	    make -j2 CXXFLAGS="-std=c++0x -fPIC" VERBOSE=true ZLIB=false GMP=false READLINE=false scipoptlib
-	    touch .completed_build
-	fi
-	cd $NMZDIR
+    autotools-scip*)
 	./bootstrap.sh || exit 1
-	./configure --enable-scip --with-scipoptsuite-src=scipoptsuite-$SCIPOPTSUITE_VERSION || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
+	./configure --enable-scip --with-scipoptsuite-src=$SCIP_BUILD_DIR/scipoptsuite-$SCIPOPTSUITE_VERSION || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
 	make -j2 || exit 1
 	OMP_NUM_THREADS=4 make -j2 check || exit 1
 	;;
     *)
-	# autotools
+	# autotools, no SCIP
 	./bootstrap.sh || exit 1
-	./configure || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
+	./configure --disable-scip || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
 	make -j2 || exit 1
 	OMP_NUM_THREADS=4 make -j2 check || exit 1
 	;;
