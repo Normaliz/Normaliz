fbd7543b22cc15c3840b260ef73aa258c6d49009
Winfried Bruns <winfried@t540p-2>
2016-11-26 02:54:43 +0100

    September -> November

--- source/normaliz.cpp
+++ source/normaliz.cpp
@@ -50,7 +50,7 @@ void printHeader() {
                                                  << "             \\....|"<<endl;
     cout << "                                                      \\...|"<<endl;
     cout << "     (C) The Normaliz Team, University of Osnabrueck   \\..|"<<endl;
-    cout << "                   September  2016                      \\.|"<<endl;
+    cout << "                    November  2016                      \\.|"<<endl;
     cout << "                                                         \\|"<<endl;
 }
 void printHelp(char* command) {
