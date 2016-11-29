056a3b603e6eca3c9a7979b0563716ad2dcc80c2
Winfried Bruns <winfried@t540p-2>
2016-11-29 18:31:21 +0100

    InzHull with MaxSubspace; LongLong in tests

--- source/libnormaliz/cone.cpp
+++ source/libnormaliz/cone.cpp
@@ -1684,8 +1684,11 @@ void Cone<Integer>::compute_integer_hull() {
         }
     }
     
+    if(!isComputed(ConeProperty::MaximalSubspace))
+        recursive_compute(ConeProperty::MaximalSubspace);
+    
     // IntHullGen.pretty_print(cout);
-    IntHullCone=new Cone<Integer>(InputType::cone_and_lattice,IntHullGen.get_elements());
+    IntHullCone=new Cone<Integer>(InputType::cone_and_lattice,IntHullGen.get_elements(), Type::subspace,BasisMaxSubspace);
     if(nr_extr!=0)  // we suppress the ordering in full_cone only if we have found few extreme rays
         IntHullCompute.set(ConeProperty::KeepOrder);
 
