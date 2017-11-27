e1eda84c72dec947fdacb2e815185bf3e9dbd3e7
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2017-11-27 22:13:58 +0100

    Added flint to autotools build

--- install_normaliz.sh
+++ install_normaliz.sh
@@ -11,11 +11,6 @@ if [ ! -e configure ];
 then
     ./bootstrap.sh
 fi
-export CPPFLAGS="-I ${NMZ_OPT_DIR}/include"
-export LDFLAGS="-L${NMZ_OPT_DIR}/lib -Wl,-rpath,${NMZ_OPT_DIR}/lib"
-export LIBS="-lflint"
-export CXXFLAGS="-DNMZ_FLINT"
-export LT_SYS_LIBRARY_PATH="${NMZ_OPT_DIR}/lib"
-./configure --with-cocoalib=${NMZ_OPT_DIR}
+./configure --with-cocoalib=${NMZ_OPT_DIR} --with-flint=${NMZ_OPT_DIR}
 make -j4
 echo "If you want to install normaliz, run sudo make install"
