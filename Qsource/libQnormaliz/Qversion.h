3e219078a39f6c1a7fd65e4f3fc2186db3a94131
Winfried Bruns <wbruns@uos.de>
2018-06-11 12:36:38 +0200

    version.h updated

--- Qsource/libQnormaliz/Qversion.h
+++ Qsource/libQnormaliz/Qversion.h
@@ -3,8 +3,8 @@
 
 #define QNMZ_VERSION_MAJOR  3
 #define QNMZ_VERSION_MINOR  6
-#define QNMZ_VERSION_PATCH  0
-#define QNMZ_VERSION        3.6.0
+#define QNMZ_VERSION_PATCH  1
+#define QNMZ_VERSION        3.6.1
 #define QNMZ_RELEASE (QNMZ_VERSION_MAJOR * 10000 + QNMZ_VERSION_MINOR * 100 + QNMZ_VERSION_PATCH)
 
 namespace libQnormaliz {
