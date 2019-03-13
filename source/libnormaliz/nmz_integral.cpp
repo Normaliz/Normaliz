34f56669d7296c52550e63c7a188948fe7b6e65a
Winfried Bruns <winfried@ryzen>
2019-03-13 14:23:31 +0100

    Update to CoCoALib 0.99601

--- source/libnormaliz/nmz_integral.cpp
+++ source/libnormaliz/nmz_integral.cpp
@@ -395,7 +395,8 @@ try{
   vector<Integer> test_grading=C.getSublattice().to_sublattice_dual_no_div(C.getGrading());
   Integer corr_factor=v_gcd(test_grading);  
   mpz_class corr_mpz=convertTo<mpz_class>(corr_factor);
-  I*=BigInt(corr_mpz.get_mpz_t());  
+  // I*=BigInt(corr_mpz.get_mpz_t());
+  I*=BigIntFromMPZ(corr_mpz.get_mpz_t());  
   
   string result="Integral";
   if(do_virt_mult)
