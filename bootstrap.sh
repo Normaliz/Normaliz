6d22d16659760f421e11cd51b2bfa618b8ab0e46
Max Horn <max@quendi.de>
2019-09-19 13:13:18 +0200

    Abort bootstrap.sh if autoreconf produces an error

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
-#! /bin/sh
+#!/bin/sh
 ## Set up the autotools build system.
 ## This script is only necessary when Normaliz is checked out from git.
 ## A distribution made using "make distcheck" will contain the complete
 ## build system already, so it is ready for "./configure && make && make install".
+set -e
 autoreconf -i --force
 echo "Now you can do:"
 echo "  ./configure && make && make install"
