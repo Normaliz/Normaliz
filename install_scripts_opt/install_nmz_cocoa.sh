5bde24975697d3568cefdd78793fd67143eba43a
Max Horn <max@quendi.de>
2020-01-09 16:26:09 +0100

    Overhaul Travis setup
    - install dependencies via apt resp. homebrew were possible, to reduce
      compilation time
    - move calls to `install_*.sh` scripts out of `.travis-build.sh` into
      `.travis.yml`; that way, we have full control over which of them to call
      when; furthermore, the resulting Travis logs are much more useful (each
      `install_*` now gets its own folded section, with timing information)
    - unified a lot of things in the Travis setup and the install scripts (this
      may lead to a few regressions initially, but I am confident we can fix any
      of those quickly)
    - install_nmz_flint.sh: turn off verbose mode

--- install_scripts_opt/install_nmz_cocoa.sh
+++ install_scripts_opt/install_nmz_cocoa.sh
@@ -2,12 +2,12 @@
 
 set -e
 
+source $(dirname "$0")/common.sh
+
 if [ "$GMP_INSTALLDIR" != "" ]; then
     CPPFLAGS="${CPFFLAGS} -I${GMP_INSTALLDIR}/include"
     LDFLAGS="${LDFLAGS} -L${GMP_INSTALLDIR}/lib"
 fi
-
-source $(dirname "$0")/common.sh
 
 ##  script for the installation of CoCoALib
 ## as far as needed by libnormaliz
@@ -18,12 +18,15 @@ COCOA_SHA256=caf37f71398b9715be262e434f04a218db05cfa58e08bce954626d7f4ffd6b75
 
 echo "Installing CoCoA..."
 
+# download & extract
 mkdir -p ${NMZ_OPT_DIR}/CoCoA_source/
 cd ${NMZ_OPT_DIR}/CoCoA_source/
 ../../download.sh ${COCOA_URL} ${COCOA_SHA256}
 if [ ! -d CoCoALib-${COCOA_VERSION} ]; then
     tar xvf CoCoALib-${COCOA_VERSION}.tgz
 fi
+
+# configure & compile
 cd CoCoALib-${COCOA_VERSION}
 if [ ! -f configuration/autoconf.mk ]; then
     ./configure --threadsafe-hack --no-boost
--- install_scripts_opt/install_nmz_nauty.sh
+++ install_scripts_opt/install_nmz_nauty.sh
@@ -2,12 +2,12 @@
 
 set -e
 
+source $(dirname "$0")/common.sh
+
 if [ "$GMP_INSTALLDIR" != "" ]; then
     CPPFLAGS="${CPFFLAGS} -I${GMP_INSTALLDIR}/include"
     LDFLAGS="${LDFLAGS} -L${GMP_INSTALLDIR}/lib"
 fi
-
-source $(dirname "$0")/common.sh
 
 ##  script for the installation of nauty
 ## as far as needed by libnormaliz
@@ -16,9 +16,9 @@ NAUTY_VERSION="27rc2"
 NAUTY_URL="http://pallini.di.uniroma1.it/nauty${NAUTY_VERSION}.tar.gz"
 NAUTY_SHA256=fe5b893579d84736a6e1383e0dfe95f9df55bff6ad3eaafd44660e08745ab32f
 
-
 echo "Installing nauty..."
 
+# download & extract
 mkdir -p ${NMZ_OPT_DIR}/Nauty_source/
 cd ${NMZ_OPT_DIR}/Nauty_source/
 ../../download.sh ${NAUTY_URL} ${NAUTY_SHA256}
@@ -27,8 +27,8 @@ if [ ! -d nauty${NAUTY_VERSION} ]; then
 fi
 cd nauty${NAUTY_VERSION}
 
+# configure & compile
 ./configure
-
 make all -j4 CFLAGS="-fPIC -O3"
 mkdir -p ${PREFIX}/include/nauty
 cp nauty.h ${PREFIX}/include/nauty
