d68a59bc668010a309f3075e3cc57d99c607e7e5
Winfried Bruns <winfried@ryzen>
2020-02-15 20:29:17 +0100

    old e-antic installation

--- install_scripts_opt/install_nmz_e-antic.sh
+++ install_scripts_opt/install_nmz_e-antic.sh
@@ -2,36 +2,35 @@
 
 set -e
 
-source $(dirname "$0")/common.sh
-
-CONFIGURE_FLAGS="--prefix=${PREFIX}"
-if [ "x$NO_OPENMP" != x ]; then
-    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --disable-openmp"
-fi
-
 if [ "$GMP_INSTALLDIR" != "" ]; then
     CPPFLAGS="${CPFFLAGS} -I${GMP_INSTALLDIR}/include"
     LDFLAGS="${LDFLAGS} -L${GMP_INSTALLDIR}/lib"
 fi
+
+source $(dirname "$0")/common.sh
 
 ## script for the installation of e-antic for the use in libnormaliz
 E_ANTIC_VERSION=0.1.3b0
 E_ANTIC_URL="http://www.labri.fr/perso/vdelecro/e-antic/e-antic-${E_ANTIC_VERSION}.tar.gz"
 E_ANTIC_SHA256=7bfe4aa926303b87a58a962535793bfadd9bbf0f7617e7be82e0f77e3351438e
 
+if [ "x$NO_OPENMP" != x ]; then
+    export BLOCK_OPENMP="--disable-openmp"
+fi
+
 echo "Installing E-ANTIC..."
 
-# download & extract
 mkdir -p ${NMZ_OPT_DIR}/E-ANTIC_source/
 cd ${NMZ_OPT_DIR}/E-ANTIC_source
 
 ../../download.sh ${E_ANTIC_URL} ${E_ANTIC_SHA256}
 if [ ! -d e-antic-${E_ANTIC_VERSION} ]; then
     tar -xvf e-antic-${E_ANTIC_VERSION}.tar.gz
+
 fi
 
-# configure & compile
 cd e-antic-${E_ANTIC_VERSION}
+
 if [ ! -f config.status ]; then
     ./configure --prefix=${PREFIX}  ${BLOCK_OPENMP} \
               CFLAGS="${CFLAGS} -I${PREFIX}/include" \
