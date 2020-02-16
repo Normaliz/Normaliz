ea3311f2873f91c67d4113b08649d493124524fd
Winfried Bruns <winfried@ryzen>
2020-02-16 19:25:31 +0100

    e-antic back to Release

--- install_scripts_opt/install_nmz_e-antic.sh
+++ install_scripts_opt/install_nmz_e-antic.sh
@@ -2,51 +2,41 @@
 
 set -e
 
-source $(dirname "$0")/common.sh
-
-echo "Prefix e-antic"
-echo ${PREFIX}
-
-echo "Start r-antic"
-ls ${PREFIX}/lib
-
-CONFIGURE_FLAGS="--prefix=${PREFIX}"
-if [ "x$NO_OPENMP" != x ]; then
-    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --disable-openmp"
-fi
-
 if [ "$GMP_INSTALLDIR" != "" ]; then
     CPPFLAGS="${CPFFLAGS} -I${GMP_INSTALLDIR}/include"
     LDFLAGS="${LDFLAGS} -L${GMP_INSTALLDIR}/lib"
 fi
+
+source $(dirname "$0")/common.sh
 
 ## script for the installation of e-antic for the use in libnormaliz
 E_ANTIC_VERSION=0.1.3b0
 E_ANTIC_URL="http://www.labri.fr/perso/vdelecro/e-antic/e-antic-${E_ANTIC_VERSION}.tar.gz"
 E_ANTIC_SHA256=7bfe4aa926303b87a58a962535793bfadd9bbf0f7617e7be82e0f77e3351438e
 
+if [ "x$NO_OPENMP" != x ]; then
+    export BLOCK_OPENMP="--disable-openmp"
+fi
+
 echo "Installing E-ANTIC..."
 
-# download & extract
 mkdir -p ${NMZ_OPT_DIR}/E-ANTIC_source/
 cd ${NMZ_OPT_DIR}/E-ANTIC_source
 
 ../../download.sh ${E_ANTIC_URL} ${E_ANTIC_SHA256}
 if [ ! -d e-antic-${E_ANTIC_VERSION} ]; then
     tar -xvf e-antic-${E_ANTIC_VERSION}.tar.gz
+
 fi
 
-# configure & compile
 cd e-antic-${E_ANTIC_VERSION}
+
 if [ ! -f config.status ]; then
     ./configure --prefix=${PREFIX}  ${BLOCK_OPENMP} \
-              CFLAGS="${CFLAGS} -I${PREFIX}/include -fPIC" \
-              CPPFLAGS="${CPPFLAGS} -fPIC" \
-              LDFLAGS="${LDFLAGS}"
+              CFLAGS="${CFLAGS} -I${PREFIX}/include" \
+              CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -fPIC" \
+              LDFLAGS="${LDFLAGS} -L/${PREFIX}/lib"
 # --enable-flint-devel ## for Flint development version
 fi
 make -j4
 make install
-
-echo "End r-antic"
-ls ${PREFIX}/lib
