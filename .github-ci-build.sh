4a09cdfa34e55e0c2a3cbf297fc0410491dcf06f
Ulrich von der Ohe <uvonderohe@uos.de>
2020-12-29 22:58:41 +0100

    try /usr/include/machine

--- .github-ci-build.sh
+++ .github-ci-build.sh
@@ -77,7 +77,7 @@ case $BUILDSYSTEM in
             rm -f ${BREWDIR}/lib/*gmp*.dylib*
             rm -f ${BREWDIR}/lib/*mpfr*.dylib*
             rm -f ${BREWDIR}/lib/*flint*.dylib*
-            export CPPFLAGS="${CPPFLAGS} -I/usr/local/machine"
+            export CPPFLAGS="${CPPFLAGS} -I/usr/include/machine"
         fi
 
         make -j2 LDFLAGS="${LDFLAGS} -all-static"
@@ -110,7 +110,7 @@ case $BUILDSYSTEM in
         make install
         if [[ $OSTYPE == darwin* ]]; then
             otool -L ${PREFIX}/bin/*
-            export CPPFLAGS="${CPPFLAGS} -I/usr/local/machine"
+            export CPPFLAGS="${CPPFLAGS} -I/usr/include/machine"
         else
             ldd ${PREFIX}/bin/*
         fi
