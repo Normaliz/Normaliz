5d81c9b4b82255684c2c0e5e98359be3dbe4437c
Ulrich von der Ohe <uvonderohe@uos.de>
2020-12-30 02:19:47 +0100

    export DYLD_LIBRARY_PATH=${OPTLIBDIR}:${DYLD_LIBRARY_PATH}

--- .github-ci-build.sh
+++ .github-ci-build.sh
@@ -86,7 +86,8 @@ case $BUILDSYSTEM in
             install -m 0644 /usr/local/opt/llvm/lib/libomp.dylib ${PREFIX}/bin
             install_name_tool -id "@loader_path/./libomp.dylib" ${PREFIX}/bin/libomp.dylib
             install_name_tool -change "/usr/local/opt/llvm/lib/libomp.dylib" "@loader_path/./libomp.dylib" ${PREFIX}/bin/normaliz
-            install_name_tool -add_rpath "${PREFIX}/lib" ${PREFIX}/bin/normaliz
+            # install_name_tool -add_rpath ${OPTLIBDIR} ${PREFIX}/bin/normaliz
+	    export DYLD_LIBRARY_PATH=${OPTLIBDIR}:${DYLD_LIBRARY_PATH}
         fi
 
         if [[ $OSTYPE == darwin* ]]; then
