4a3d94426b1d46ca476137221a0ff89d1b29bfad
Ulrich von der Ohe <uvonderohe@uos.de>
2020-12-30 21:40:23 +0100

    Separate configure, make, and check stages for GitHub Actions.

---
+++ .github-ci-build_configure.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e # exit on errors
+set -x # print commands and their arguments as they are executed
+
+if [[ $BUILDSYSTEM != *static* ]]; then
+    if [[ $OSTYPE == darwin* ]]; then 
+        export NMZ_COMPILER=clang++
+    fi
+fi
+
+source install_scripts_opt/common.sh
+
+# Prepare configure flags
+CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --prefix=${PREFIX}"
+
+if [ "x$NO_OPENMP" != x ]; then
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --disable-openmp"
+fi
+
+# install dependencies
+if [[ $BUILDSYSTEM == *nauty* ]]; then
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --with-nauty"
+else
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --without-nauty"
+fi
+
+if [[ $BUILDSYSTEM == *flint* || $BUILDSYSTEM == *eantic* ]]; then
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --with-flint"
+else
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --without-flint"
+fi
+
+if [[ $BUILDSYSTEM == *eantic* ]]; then
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --with-e-antic"
+else
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --without-e-antic"
+fi
+
+if [[ $BUILDSYSTEM == *cocoa* ]]; then
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --with-cocoalib"
+else
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --without-cocoalib"
+fi
+
+# Build Normaliz.
+./bootstrap.sh
+
+if [[ -z $NO_COVERAGE ]]; then
+    export CFLAGS="${CFLAGS} --coverage -O2 -g"
+    export CXXFLAGS="${CXXFLAGS} --coverage -O2 -g"
+    export LDFLAGS="${LDFLAGS} --coverage"
+fi
+
+if [[ $BUILDSYSTEM == *extended* ]]; then
+    export CPPFLAGS="${CPPFLAGS} -DNMZ_EXTENDED_TESTS"
+fi
+
+./configure ${CONFIGURE_FLAGS} || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
+
+# Have normaliz testsuite print running time:
+export NICE=time
+
+# Limit number of threads
+export OMP_NUM_THREADS=4
+
+export -p | sed 's/declare -x/export/g' > VARS1
+echo "environment variables stored in VARS1 -- this file gets sourced by the succeeding script"
---
+++ .github-ci-build_make.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+source VARS1
+
+case $BUILDSYSTEM in
+
+    *static*)
+        OPTLIBDIR=${PREFIX}/lib
+
+        # Remove shared libraries and libtool *.la files to force static linking
+        # ls -laR ${OPTLIBDIR}
+        rm -f ${OPTLIBDIR}/*.dylib*
+        rm -f ${OPTLIBDIR}/*.so*
+        rm -f ${OPTLIBDIR}/*.la
+        # ls -laR ${OPTLIBDIR}
+        if [[ $OSTYPE == darwin* ]]; then
+            BREWDIR=$(brew --prefix)
+            rm -f ${BREWDIR}/lib/*gmp*.dylib*
+            rm -f ${BREWDIR}/lib/*mpfr*.dylib*
+            rm -f ${BREWDIR}/lib/*flint*.dylib*
+        fi
+
+        make -j2 LDFLAGS="${LDFLAGS} -all-static"
+        make install
+        ;;
+
+    *makedistcheck*)
+        # make -j2 distcheck # checks in succeeding script
+        ;;
+
+    *)
+        make -j2 -k
+        # make -j2 -k check
+        # make install
+        # if [[ $OSTYPE == darwin* ]]; then
+        #     otool -L ${PREFIX}/bin/*
+        # else
+        #     ldd ${PREFIX}/bin/*
+        # fi
+        # make installcheck
+        ;;
+esac
+
+export -p | sed 's/declare -x/export/g' > VARS2
+echo "environment variables stored in VARS2 -- this file gets sourced by the succeeding script"
