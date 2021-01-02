6c3ed0dbc732993594d40ecd5bfbc23e76611f5b
Winfried Bruns <winfried@ryzen>
2021-01-02 15:45:32 +0100

    Now make distcheck in yml

---
+++ .github-ci-build_check.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+source VARS2
+
+case $BUILDSYSTEM in
+
+    *static*)
+        if [[ $OSTYPE == darwin* ]]; then
+            install -m 0644 /usr/local/opt/llvm/lib/libomp.dylib ${PREFIX}/bin
+            otool -L ${PREFIX}/bin/normaliz
+            install_name_tool -id "@loader_path/./libomp.dylib" ${PREFIX}/bin/libomp.dylib
+            install_name_tool -change "/usr/local/opt/llvm/lib/libomp.dylib" "@loader_path/./libomp.dylib" ${PREFIX}/bin/normaliz
+        fi
+
+        if [[ $OSTYPE == darwin* ]]; then
+            otool -L ${PREFIX}/bin/*
+        else
+            ldd ${PREFIX}/bin/*
+        fi
+        
+        export NORMPARA=-x=1 ## more is not possible on GitHub Mac
+
+        make check
+        
+        cd local/bin
+        echo "CONTENTS OF LOCAL/BIN"
+        ls .
+        zip MacOSbinary.zip *
+        ;;
+
+    *makedistcheck*)
+        make -j2 distcheck
+        ;;
+
+    *)
+        # make -j2 -k
+        make -j2 -k check
+        make install
+        if [[ $OSTYPE == darwin* ]]; then
+            otool -L ${PREFIX}/bin/*
+        else
+            ldd ${PREFIX}/bin/*
+        fi
+        make installcheck
+        ;;
+esac
+
+export -p | sed 's/declare -x/export/g' > VARS3
