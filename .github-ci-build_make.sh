d6bec1483c494d7fcac2c277b4ebf69eaaf732dc
Julian Rüth <julian.rueth@fsfe.org>
2021-01-24 15:47:01 +0100

    Let libtool find indirect dependencies
    when building -all-static, libtool pulls in the static versions of
    libraries that describe them in their .la files. Unfortunately, libtools
    is not so widely used, so to use that feature we have to create fake
    libtool files.
    
    However, this does not work: the macOS linker cannot link static
    archives that define the same object twice. E-ANTIC needs to redefine a
    symbol that is broken in some versions of ANTIC. On Linux this is not an
    issue since we are hiding that symbol with a version map. Or we can use
    --allow-multiple-definition. On macOS there is no such option (anymore)
    so we can only work around this with dynamic linking.

--- .github-ci-build_make.sh
+++ .github-ci-build_make.sh
@@ -7,25 +7,12 @@ if [ -e VARS1 ]; then
 fi
 
 case $BUILDSYSTEM in
-
     *static*)
-        OPTLIBDIR=${PREFIX}/lib
-
-        # Remove shared libraries and libtool *.la files to force static linking
-        # ls -laR ${OPTLIBDIR}
-        rm -f ${OPTLIBDIR}/*.dylib*
-        rm -f ${OPTLIBDIR}/*.so*
-        rm -f ${OPTLIBDIR}/*.la
-        # ls -laR ${OPTLIBDIR}
-        if [[ $OSTYPE == darwin* ]]; then
-            BREWDIR=$(brew --prefix)
-            rm -f ${BREWDIR}/lib/*gmp*.dylib*
-            rm -f ${BREWDIR}/lib/*mpfr*.dylib*
-            rm -f ${BREWDIR}/lib/*flint*.dylib*
-        fi
-
-        make -j2 LDFLAGS="${LDFLAGS} -all-static"
+        make -j2
         make install
+        if [[ $OSTYPE == darwin* ]]; then
+            otool -L ${PREFIX}/bin/normaliz
+        fi
         ;;
 
     *makedistcheck*)
