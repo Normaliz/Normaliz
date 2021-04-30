34c7cf340f3eb2f18d8ee7206a85bae6382a35e2
Winfried Bruns <winfried@ryzen>
2021-04-30 12:06:32 +0200

    Tests with fixed precision moved

--- source/chunk/scheduler.cpp
+++ source/chunk/scheduler.cpp
@@ -38,38 +38,57 @@ int main(int argc, char* argv[]) {
     long max_simultaneously = stoi(string(argv[3]));
     long nr_threads = stoi(string(argv[4]));
     
-    set<long> jobs;
-    for(long i = start_job; i<=end_job;++i)
-        jobs.insert(i);
-    
     vector<bool> done(end_job+1), running(end_job+1);
-    bool all_done = false;
-    long nr_running = 0;
     long nr_done = 0;
+    for(long i=0; i<start_job;++i){
+        nr_done++;
+        done[i] = true;
+    }
+
+    for(long j=start_job; j <=end_job;++j){
+        string file_name = "hollow_tri."; // discard non-existing input files
+        if(! exists_file(file_name + to_string(j)+".gz") ){
+            done[j] = true;
+            nr_done++;
+            continue;
+        }
+        file_name = "mult."; // discard done jobs
+        if(exists_file(file_name + to_string(j))){
+            done[j] = true;
+            nr_done++;
+            continue;
+        }
+    }
+    
+    if(nr_done == end_job - start_job +1){
+            cout << "Nothing to do" << endl;
+            exit(0);
+    }
+    
+    long nr_running = 0;
     
     while(true){
         for(long j=start_job; j <=end_job;++j){
             if(done[j])
-                continue;
+                continue;                      
             string file_name = "mult.";
-            if(running[j] && exists_file(file_name + to_string(j))){
-                running[j] = false;
-                nr_running--;
-                done[j] = true;;
+            if(exists_file(file_name + to_string(j))){
+                done[j] = true;
                 nr_done++;
+                if(running[j]){
+                    nr_running--;
+                    running[j] = false;
+                }
+                continue;
             }
             std::this_thread::sleep_for (std::chrono::seconds(3));
-            if(nr_done == end_job - start_job +1){
-                all_done = true;
-                break;
-            }
-            if(nr_running < max_simultaneously && jobs.size() > 0){
-                long to_be_done = *(jobs.begin());
-                jobs.erase(to_be_done);
+
+            if(nr_running < max_simultaneously && !running[j]){
+                long to_be_done = j;
                 running[to_be_done] = true;
+                nr_running++;
                 string command = "nohup ../run_single.sh " + to_string(to_be_done) + " " + to_string(nr_threads) + " &>> " + "chunk.log."
                 + to_string(to_be_done) +" &";
-                nr_running++;
                 int failure = system(command.c_str());
                 if(failure > 0){
                     cout << "Command ended with exit code > 0";
@@ -79,8 +98,11 @@ int main(int argc, char* argv[]) {
             }
             
         }
-        if(all_done)
-            break;
+        if(nr_done == end_job - start_job +1 && nr_running == 0){
+                cout << "All jobs done" << endl;
+                exit(0);
+        }
+    
         std::this_thread::sleep_for (std::chrono::seconds(3));
         
     }    
