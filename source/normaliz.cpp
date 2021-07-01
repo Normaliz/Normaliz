1c0f34ec1fb55ad6ac246cb5342ee6250423e2a8
Winfried Bruns <wbruns@uos.de>
2021-07-01 15:52:38 +0200

    Date corrected

--- source/normaliz.cpp
+++ source/normaliz.cpp
@@ -58,7 +58,7 @@ void printHeader() {
 #endif
     cout << "                                                       \\...|" << endl;
     cout << "     (C) The Normaliz Team, University of Osnabrueck    \\..|" << endl;
-    cout << "                      March 2021                         \\.|" << endl;
+    cout << "                        July 2021                        \\.|" << endl;
     cout << "                                                          \\|" << endl;
     string optional_packages = package_string();
     if (optional_packages.size() >0 ) {
