7e4f194ed9ab570ce64e4d11b4e7b874d0896ff9
Winfried Bruns <winfried@ryzen>
2021-08-31 18:21:57 +0200

    Repair operator < in danamic_bitset

--- source/libnormaliz/dynamic_bitset.h
+++ source/libnormaliz/dynamic_bitset.h
@@ -324,8 +324,13 @@ class dynamic_bitset {
         return !(*this == x);
     }
     bool operator<(const dynamic_bitset& x) const {  // for use in std::set, std::map
-        if (_total_bits == x._total_bits)
-            return _limbs < x._limbs;
+        if (_total_bits == x._total_bits) {
+            for (size_t i = 1; i <= _limbs.size(); ++i) {
+                if (_limbs[_limbs.size() - i] != x._limbs[_limbs.size() - i])
+                    return (_limbs[_limbs.size() - i] < x._limbs[_limbs.size() - i]);
+            }
+            return false;
+        }
         return _total_bits < x._total_bits;
     }
 
