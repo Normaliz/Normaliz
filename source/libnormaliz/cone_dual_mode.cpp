d03b9e80a9f5f2f2fcb5567f1c44274390ebe5e8
Winfried Bruns <wbruns@uos.de>
2022-02-05 11:43:20 +0100

    Clean up in cone_dual_mode.cpp

--- source/libnormaliz/cone_dual_mode.cpp
+++ source/libnormaliz/cone_dual_mode.cpp
@@ -668,11 +668,7 @@ void Cone_Dual_Mode<Integer>::cut_with_halfspace_hilbert_basis(const size_t& hyp
         if (!New_Neutral_Irred.empty()) {
             if (do_reduction) {
                 Positive_Depot.reduce_by(New_Neutral_Irred);
-                cout << "IIIIIIIIIIIIIIIII " << New_Neutral_Irred.size() << endl;
-                cout << "DDDDDDDDDDDDDDDDD " << Neutral_Depot.size() << endl;
                 Neutral_Depot.reduce_by(New_Neutral_Irred);
-                cout << "EEEEEEEEEEEEEEEEE " << Neutral_Depot.size() << endl;
-                cout << "+++++++++++++++++++++++++++++++++++++++++++ " << endl;
             }
             Negative_Depot.reduce_by(New_Neutral_Irred);
             list<Candidate<Integer>*> New_Elements;
