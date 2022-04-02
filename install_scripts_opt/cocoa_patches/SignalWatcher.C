2372d92ffccab2536beb27f649de5653a347ffd3
Max Horn <max@quendi.de>
2022-04-02 11:59:30 +0200

    Update some URLs from http to https

--- install_scripts_opt/cocoa_patches/SignalWatcher.C
+++ install_scripts_opt/cocoa_patches/SignalWatcher.C
@@ -13,7 +13,7 @@
 //   GNU General Public License for more details.
 
 //   You should have received a copy of the GNU General Public License
-//   along with CoCoALib.  If not, see <http://www.gnu.org/licenses/>.
+//   along with CoCoALib.  If not, see <https://www.gnu.org/licenses/>.
 
 
 #include "CoCoA/SignalWatcher.H"
