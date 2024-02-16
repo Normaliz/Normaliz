15c5f03e888a31969355f02229ec6bd92ffb90d2
Winfried Bruns <wbruns@uos.de>
2024-02-16 09:31:41 +0100

    No arb and antic in static version anymore

--- .github/workflows/.github-ci-build_configure.sh
+++ .github/workflows/.github-ci-build_configure.sh
@@ -44,9 +44,9 @@ fi
 
 if [[ $BUILDSYSTEM == *static* ]]; then
     CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --disable-shared"
-    if [[ $BUILDSYSTEM == *eantic* ]]; then
-      sed -ie s/-leanticxx\ -leantic/-leanticxx\ -leantic\ -lantic\ -larb/g configure.ac
-    fi
+    ## if [[ $BUILDSYSTEM == *eantic* ]]; then
+    ##  sed -ie s/-leanticxx\ -leantic/-leanticxx\ -leantic\ -lantic\ -larb/g configure.ac
+    ## fi
 fi
 
 
