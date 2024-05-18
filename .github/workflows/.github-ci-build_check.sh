9d46f987f8d658de4d65d49d10ee5b5b957fd04f
Winfried Bruns <wbruns@uos.de>
2024-05-18 10:28:50 +0200

    Now with /usr/local/lib for libomp.dylib

--- .github/workflows/.github-ci-build_check.sh
+++ .github/workflows/.github-ci-build_check.sh
@@ -10,15 +10,10 @@ case $BUILDSYSTEM in
 
     *static*)
         if [[ $OSTYPE == darwin* ]]; then
-            echo "********************** "
-            echo $(brew --prefix)
-            echo "********************** "
-            # install -m 0644 /usr/local/opt/llvm/lib/libomp.dylib ${PREFIX}/bin
-            install -m 0644 $(brew --prefix)/lib/libomp.dylib ${PREFIX}/bin
+            install -m 0644 /usr/local/lib/libomp.dylib ${PREFIX}/bin
             otool -L ${PREFIX}/bin/normaliz
             install_name_tool -id "@loader_path/./libomp.dylib" ${PREFIX}/bin/libomp.dylib
-            #install_name_tool -change "/usr/local/opt/llvm/lib/libomp.dylib" "@loader_path/./libomp.dylib" ${PREFIX}/bin/normaliz
-            install_name_tool -change "$(brew --prefix)/lib/libomp.dylib" "@loader_path/./libomp.dylib" ${PREFIX}/bin/normaliz
+            install_name_tool -change "/usr/local/lib/libomp.dylib" "@loader_path/./libomp.dylib" ${PREFIX}/bin/normaliz
         fi
 
         if [[ $OSTYPE == darwin* ]]; then
