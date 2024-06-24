2313f40d609aadb16e328a35a4d74b46c2e13aa0
Winfried Bruns <wbruns@uos.de>
2024-06-24 10:25:44 +0200

    correction for libomp.dylib binding zo normaliz

--- .github/workflows/.github-ci-build_check.sh
+++ .github/workflows/.github-ci-build_check.sh
@@ -13,7 +13,8 @@ case $BUILDSYSTEM in
            install -m 0644 $(brew --prefix)/opt/libomp/lib/libomp.dylib ${PREFIX}/bin
            otool -L ${PREFIX}/bin/normaliz
            install_name_tool -id "@loader_path/./libomp.dylib" ${PREFIX}/bin/libomp.dylib
-           install_name_tool -change "$(brew --prefix)$(brew --prefix)/opt/libomp/lib/libomp.dylib" "@loader_path/./libomp.dylib" ${PREFIX}/bin/normaliz
+           install_name_tool -change "$(brew --prefix)/opt/libomp/lib/libomp.dylib" "@loader_path/./libomp.dylib" ${PREFIX}/bin/normaliz
+           otool -L ${PREFIX}/bin/normaliz
          fi
 
         if [[ $OSTYPE == darwin* ]]; then
