5c5b95e0265ac1159347bb63a40c846df345bb0e
Winfried Bruns <wbruns@uos.de>
2024-11-04 12:38:50 +0100

    One step back

--- install_scripts_opt/common.sh
+++ install_scripts_opt/common.sh
@@ -11,7 +11,7 @@ if [ "x$NMZ_COMPILER" != x ]; then
     export CXX=$NMZ_COMPILER
 elif [[ $OSTYPE == darwin* ]]; then   ## activate Homebrew LLVM
     LLVMDIR="$(brew --prefix)/opt/llvm"
-    export LDFLAGS="${LDFLAGS} $(brew --prefix)/opt/libomp/lib -L${LLVMDIR}/lib -Wl,-rpath,${LLVMDIR}/lib"
+    export LDFLAGS="${LDFLAGS} -L${LLVMDIR}/lib -Wl,-rpath,${LLVMDIR}/lib"
     export CPPFLAGS="${CPPFLAGS} -I ${LLVMDIR}/include"
     export PATH="${LLVMDIR}/bin/:$PATH"
     export CXX=clang++
