ef7e851968b5fe0ebaf7d3960c22ea36f3ac5fac
Winfried Bruns <wbruns@uos.de>
2026-01-30 12:26:24 +0100

    Tests Perm --> perm

--- test/out2ref.sh
+++ test/out2ref.sh
@@ -7,8 +7,24 @@ mkdir -p copy
 cd copy
 cp ../*.out .
 cp ../*.aut .
+cp ../*.ind .
+cp ../*.cst .
+cp ../*.tri .
+cp ../*.fac .
+cp ../*.gen .
+cp ../*.grb .
+cp ../*.inc .
+cp ../*.mrk .
 rename 's/\.out$/.ref/' *.out
 rename 's/\.aut$/.aut.ref/' *.aut
+rename 's/\.ind$/.ind.ref/' *.ind
+rename 's/\.cst$/.cst.ref/' *.cst
+rename 's/\.tri$/.tri.ref/' *.tri
+rename 's/\.fac$/.fac.ref/' *.fac
+rename 's/\.gen$/.gen.ref/' *.gen
+rename 's/\.grb$/.grb.ref/' *.grb
+rename 's/\.inc$/.inc.ref/' *.inc
+rename 's/\.mrk$/.mrk.ref/' *.mrk
 cp *.ref ..
 rm *.ref
 cd ..
